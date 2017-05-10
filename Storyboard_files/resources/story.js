1a829b063e96e2d577e5b4b97c7ff9668a3a2d33
Nguyen Duong The Anh <anhndt@Nguyens-MacBook-Pro.local>
2017-05-10 21:56:13 +0700

    update

---
+++ Storyboard_files/resources/story.js
@@ -0,0 +1,1486 @@
+var story = {
+ "pages": [
+  {
+   "image": "1.Splash.png",
+   "image2x": "1.Splash@2x.png",
+   "width": 375,
+   "links": [{
+    "rect": [
+     0,
+     0,
+     375,
+     795
+    ],
+    "page": 1
+   }],
+   "title": "1.Splash",
+   "height": 795
+  },
+  {
+   "image": "2.Login.png",
+   "image2x": "2.Login@2x.png",
+   "width": 375,
+   "links": [
+    {
+     "rect": [
+      80,
+      334,
+      313,
+      379
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      80,
+      392,
+      313,
+      437
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      80,
+      446,
+      313,
+      491
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      80,
+      502,
+      313,
+      547
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      92,
+      652,
+      294,
+      675
+     ],
+     "page": 18
+    }
+   ],
+   "title": "2.Login",
+   "height": 795
+  },
+  {
+   "image": "3.Home-Discover.png",
+   "image2x": "3.Home-Discover@2x.png",
+   "width": 375,
+   "links": [
+    {
+     "rect": [
+      29,
+      453,
+      349,
+      553
+     ],
+     "page": 11
+    },
+    {
+     "rect": [
+      29,
+      553,
+      349,
+      633
+     ],
+     "page": 11
+    },
+    {
+     "rect": [
+      167,
+      637,
+      218,
+      673
+     ],
+     "page": 7
+    },
+    {
+     "rect": [
+      43,
+      644,
+      79,
+      672
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      95,
+      644,
+      136,
+      672
+     ],
+     "page": 6
+    },
+    {
+     "rect": [
+      239,
+      644,
+      280,
+      672
+     ],
+     "page": 9
+    },
+    {
+     "rect": [
+      296,
+      644,
+      330,
+      669
+     ],
+     "page": 10
+    },
+    {
+     "rect": [
+      78,
+      405,
+      110,
+      437
+     ],
+     "page": 13
+    },
+    {
+     "rect": [
+      113,
+      409,
+      301,
+      432
+     ],
+     "page": 13
+    }
+   ],
+   "title": "3.Home-Discover",
+   "height": 795
+  },
+  {
+   "image": "10.PostNewReview.png",
+   "image2x": "10.PostNewReview@2x.png",
+   "width": 375,
+   "links": [
+    {
+     "rect": [
+      34,
+      144,
+      102,
+      172
+     ],
+     "page": 11
+    },
+    {
+     "rect": [
+      29,
+      641,
+      349,
+      686
+     ],
+     "page": 11
+    }
+   ],
+   "title": "10.PostNewReview",
+   "height": 795
+  },
+  {
+   "image": "11.Activity-Detail.png",
+   "image2x": "11.Activity-Detail@2x.png",
+   "width": 375,
+   "links": [{
+    "rect": [
+     34,
+     144,
+     102,
+     172
+    ],
+    "page": 7
+   }],
+   "title": "11.Activity-Detail",
+   "height": 795
+  },
+  {
+   "image": "12.Create-Activity.png",
+   "image2x": "12.Create-Activity@2x.png",
+   "width": 375,
+   "links": [
+    {
+     "rect": [
+      34,
+      144,
+      102,
+      172
+     ],
+     "page": 7
+    },
+    {
+     "rect": [
+      296,
+      144,
+      339,
+      172
+     ],
+     "page": 7
+    },
+    {
+     "rect": [
+      116,
+      445,
+      145,
+      472
+     ],
+     "page": 19
+    }
+   ],
+   "title": "12.Create-Activity",
+   "height": 795
+  },
+  {
+   "image": "4.Home-Favourites.png",
+   "image2x": "4.Home-Favourites@2x.png",
+   "width": 375,
+   "links": [
+    {
+     "rect": [
+      167,
+      636,
+      218,
+      674
+     ],
+     "page": 7
+    },
+    {
+     "rect": [
+      42,
+      642,
+      78,
+      671
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      94,
+      642,
+      135,
+      671
+     ],
+     "page": 24
+    },
+    {
+     "rect": [
+      240,
+      642,
+      281,
+      671
+     ],
+     "page": 9
+    },
+    {
+     "rect": [
+      298,
+      642,
+      332,
+      669
+     ],
+     "page": 22
+    },
+    {
+     "rect": [
+      29,
+      187,
+      349,
+      287
+     ],
+     "page": 11
+    },
+    {
+     "rect": [
+      29,
+      287,
+      349,
+      387
+     ],
+     "page": 11
+    },
+    {
+     "rect": [
+      29,
+      387,
+      349,
+      488
+     ],
+     "page": 11
+    }
+   ],
+   "title": "4.Home-Favourites",
+   "height": 795
+  },
+  {
+   "image": "5.Home-Activitys-List.png",
+   "image2x": "5.Home-Activitys-List@2x.png",
+   "width": 375,
+   "links": [
+    {
+     "rect": [
+      167,
+      630,
+      218,
+      673
+     ],
+     "page": 7
+    },
+    {
+     "rect": [
+      43,
+      636,
+      79,
+      670
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      95,
+      636,
+      136,
+      670
+     ],
+     "page": 24
+    },
+    {
+     "rect": [
+      239,
+      636,
+      280,
+      670
+     ],
+     "page": 9
+    },
+    {
+     "rect": [
+      296,
+      636,
+      330,
+      667
+     ],
+     "page": 22
+    },
+    {
+     "rect": [
+      287,
+      144,
+      324,
+      172
+     ],
+     "page": 5
+    },
+    {
+     "rect": [
+      29,
+      224,
+      349,
+      341
+     ],
+     "page": 4
+    },
+    {
+     "rect": [
+      29,
+      348,
+      349,
+      464
+     ],
+     "page": 4
+    },
+    {
+     "rect": [
+      29,
+      464,
+      349,
+      581
+     ],
+     "page": 4
+    },
+    {
+     "rect": [
+      187,
+      180,
+      300,
+      224
+     ],
+     "page": 8
+    }
+   ],
+   "title": "5.Home-Activitys-List",
+   "height": 795
+  },
+  {
+   "image": "5.Home-Activitys-Maps.png",
+   "image2x": "5.Home-Activitys-Maps@2x.png",
+   "width": 375,
+   "links": [
+    {
+     "rect": [
+      167,
+      637,
+      218,
+      675
+     ],
+     "page": 7
+    },
+    {
+     "rect": [
+      43,
+      643,
+      79,
+      672
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      95,
+      643,
+      136,
+      672
+     ],
+     "page": 24
+    },
+    {
+     "rect": [
+      239,
+      643,
+      280,
+      672
+     ],
+     "page": 9
+    },
+    {
+     "rect": [
+      296,
+      643,
+      330,
+      670
+     ],
+     "page": 22
+    },
+    {
+     "rect": [
+      287,
+      144,
+      324,
+      172
+     ],
+     "page": 5
+    },
+    {
+     "rect": [
+      81,
+      180,
+      192,
+      224
+     ],
+     "page": 7
+    },
+    {
+     "rect": [
+      131,
+      280,
+      262,
+      345
+     ],
+     "page": 4
+    },
+    {
+     "rect": [
+      218,
+      368,
+      349,
+      433
+     ],
+     "page": 4
+    },
+    {
+     "rect": [
+      91,
+      464,
+      222,
+      529
+     ],
+     "page": 4
+    }
+   ],
+   "title": "5.Home-Activitys-Maps",
+   "height": 795
+  },
+  {
+   "image": "6.Home-Friends.png",
+   "image2x": "6.Home-Friends@2x.png",
+   "width": 375,
+   "links": [
+    {
+     "rect": [
+      167,
+      631,
+      218,
+      674
+     ],
+     "page": 7
+    },
+    {
+     "rect": [
+      43,
+      638,
+      79,
+      671
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      95,
+      638,
+      136,
+      671
+     ],
+     "page": 24
+    },
+    {
+     "rect": [
+      239,
+      638,
+      280,
+      671
+     ],
+     "page": 9
+    },
+    {
+     "rect": [
+      296,
+      638,
+      330,
+      669
+     ],
+     "page": 22
+    },
+    {
+     "rect": [
+      287,
+      144,
+      324,
+      172
+     ],
+     "page": 14
+    },
+    {
+     "rect": [
+      29,
+      241,
+      349,
+      316
+     ],
+     "page": 15
+    },
+    {
+     "rect": [
+      29,
+      316,
+      349,
+      392
+     ],
+     "page": 15
+    },
+    {
+     "rect": [
+      29,
+      392,
+      349,
+      469
+     ],
+     "page": 15
+    },
+    {
+     "rect": [
+      29,
+      180,
+      349,
+      241
+     ],
+     "page": 17
+    }
+   ],
+   "title": "6.Home-Friends",
+   "height": 795
+  },
+  {
+   "image": "7.Home-Profile.png",
+   "image2x": "7.Home-Profile@2x.png",
+   "width": 375,
+   "links": [
+    {
+     "rect": [
+      167,
+      636,
+      218,
+      676
+     ],
+     "page": 7
+    },
+    {
+     "rect": [
+      43,
+      642,
+      79,
+      672
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      95,
+      642,
+      136,
+      672
+     ],
+     "page": 24
+    },
+    {
+     "rect": [
+      239,
+      642,
+      280,
+      672
+     ],
+     "page": 9
+    },
+    {
+     "rect": [
+      296,
+      642,
+      330,
+      670
+     ],
+     "page": 22
+    }
+   ],
+   "title": "7.Home-Profile",
+   "height": 795
+  },
+  {
+   "image": "8.Location-Place-Info.png",
+   "image2x": "8.Location-Place-Info@2x.png",
+   "width": 375,
+   "links": [
+    {
+     "rect": [
+      34,
+      126,
+      102,
+      154
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      39,
+      444,
+      339,
+      598
+     ],
+     "page": 12
+    },
+    {
+     "rect": [
+      39,
+      604,
+      339,
+      758
+     ],
+     "page": 12
+    },
+    {
+     "rect": [
+      29,
+      713,
+      339,
+      758
+     ],
+     "page": 3
+    }
+   ],
+   "title": "8.Location-Place-Info",
+   "height": 861
+  },
+  {
+   "image": "9.ReviewDetail.png",
+   "image2x": "9.ReviewDetail@2x.png",
+   "width": 375,
+   "links": [{
+    "rect": [
+     29,
+     136,
+     97,
+     164
+    ],
+    "page": 11
+   }],
+   "title": "9.ReviewDetail",
+   "height": 795
+  },
+  {
+   "image": "13.Add-New-Place+Location.png",
+   "image2x": "13.Add-New-Place+Location@2x.png",
+   "width": 375,
+   "links": [
+    {
+     "rect": [
+      34,
+      144,
+      102,
+      172
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      296,
+      145,
+      339,
+      173
+     ],
+     "page": 2
+    }
+   ],
+   "title": "13.Add-New-Place+Location",
+   "height": 795
+  },
+  {
+   "image": "14.Friends-Searching.png",
+   "image2x": "14.Friends-Searching@2x.png",
+   "width": 375,
+   "links": [
+    {
+     "rect": [
+      304,
+      186,
+      336,
+      209
+     ],
+     "page": 20
+    },
+    {
+     "rect": [
+      29,
+      228,
+      349,
+      304
+     ],
+     "page": 15
+    },
+    {
+     "rect": [
+      29,
+      304,
+      349,
+      380
+     ],
+     "page": 15
+    },
+    {
+     "rect": [
+      29,
+      380,
+      349,
+      457
+     ],
+     "page": 15
+    },
+    {
+     "rect": [
+      34,
+      144,
+      102,
+      172
+     ],
+     "page": 9
+    },
+    {
+     "rect": [
+      287,
+      144,
+      334,
+      172
+     ],
+     "page": 9
+    }
+   ],
+   "title": "14.Friends-Searching",
+   "height": 795
+  },
+  {
+   "image": "15. Friend-Detail-Info.png",
+   "image2x": "15. Friend-Detail-Info@2x.png",
+   "width": 375,
+   "links": [
+    {
+     "rect": [
+      34,
+      144,
+      102,
+      172
+     ],
+     "page": 14
+    },
+    {
+     "rect": [
+      179,
+      338,
+      349,
+      365
+     ],
+     "page": 16
+    }
+   ],
+   "title": "15. Friend-Detail-Info",
+   "height": 795
+  },
+  {
+   "image": "16. Friend-Detail-Favourites.png",
+   "image2x": "16. Friend-Detail-Favourites@2x.png",
+   "width": 375,
+   "links": [
+    {
+     "rect": [
+      34,
+      144,
+      102,
+      172
+     ],
+     "page": 14
+    },
+    {
+     "rect": [
+      30,
+      315,
+      180,
+      342
+     ],
+     "page": 15
+    },
+    {
+     "rect": [
+      29,
+      343,
+      349,
+      443
+     ],
+     "page": 11
+    },
+    {
+     "rect": [
+      29,
+      443,
+      349,
+      543
+     ],
+     "page": 11
+    },
+    {
+     "rect": [
+      29,
+      543,
+      349,
+      644
+     ],
+     "page": 11
+    }
+   ],
+   "title": "16. Friend-Detail-Favourites",
+   "height": 795
+  },
+  {
+   "image": "17.Friends - Requests.png",
+   "image2x": "17.Friends - Requests@2x.png",
+   "width": 375,
+   "links": [
+    {
+     "rect": [
+      29,
+      180,
+      349,
+      256
+     ],
+     "page": 15
+    },
+    {
+     "rect": [
+      29,
+      256,
+      349,
+      332
+     ],
+     "page": 15
+    },
+    {
+     "rect": [
+      29,
+      332,
+      349,
+      409
+     ],
+     "page": 15
+    }
+   ],
+   "title": "17.Friends - Requests",
+   "height": 795
+  },
+  {
+   "image": "18.Register.png",
+   "image2x": "18.Register@2x.png",
+   "width": 375,
+   "links": [
+    {
+     "rect": [
+      29,
+      640,
+      349,
+      685
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      34,
+      144,
+      102,
+      172
+     ],
+     "page": 1
+    }
+   ],
+   "title": "18.Register",
+   "height": 795
+  },
+  {
+   "image": "19.Invite-Friend-Activity.png",
+   "image2x": "19.Invite-Friend-Activity@2x.png",
+   "width": 375,
+   "links": [
+    {
+     "rect": [
+      34,
+      144,
+      102,
+      172
+     ],
+     "page": 5
+    },
+    {
+     "rect": [
+      287,
+      144,
+      334,
+      172
+     ],
+     "page": 5
+    }
+   ],
+   "title": "19.Invite-Friend-Activity",
+   "height": 795
+  },
+  {
+   "image": "20.Friends-Searching-Filter.png",
+   "image2x": "20.Friends-Searching-Filter@2x.png",
+   "width": 375,
+   "links": [
+    {
+     "rect": [
+      39,
+      478,
+      84,
+      501
+     ],
+     "page": 14
+    },
+    {
+     "rect": [
+      315,
+      478,
+      337,
+      501
+     ],
+     "page": 14
+    },
+    {
+     "rect": [
+      168,
+      478,
+      214,
+      501
+     ],
+     "page": 14
+    },
+    {
+     "rect": [
+      29,
+      228,
+      349,
+      304
+     ],
+     "page": 15
+    },
+    {
+     "rect": [
+      29,
+      304,
+      349,
+      380
+     ],
+     "page": 15
+    },
+    {
+     "rect": [
+      29,
+      380,
+      349,
+      457
+     ],
+     "page": 15
+    },
+    {
+     "rect": [
+      34,
+      144,
+      102,
+      172
+     ],
+     "page": 9
+    },
+    {
+     "rect": [
+      287,
+      144,
+      334,
+      172
+     ],
+     "page": 9
+    }
+   ],
+   "title": "20.Friends-Searching-Filter",
+   "height": 795
+  },
+  {
+   "image": "21.Add place from Map.png",
+   "image2x": "21.Add place from Map@2x.png",
+   "width": 375,
+   "links": [
+    {
+     "rect": [
+      163,
+      634,
+      214,
+      672
+     ],
+     "page": 7
+    },
+    {
+     "rect": [
+      37,
+      641,
+      72,
+      670
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      91,
+      641,
+      131,
+      670
+     ],
+     "page": 24
+    },
+    {
+     "rect": [
+      237,
+      641,
+      277,
+      670
+     ],
+     "page": 9
+    },
+    {
+     "rect": [
+      294,
+      641,
+      328,
+      669
+     ],
+     "page": 22
+    },
+    {
+     "rect": [
+      121,
+      373,
+      256,
+      422
+     ],
+     "page": 13
+    }
+   ],
+   "title": "21.Add place from Map",
+   "height": 795
+  },
+  {
+   "image": "22. User Profile Favorites.png",
+   "image2x": "22. User Profile Favorites@2x.png",
+   "width": 375,
+   "links": [
+    {
+     "rect": [
+      287,
+      144,
+      342,
+      172
+     ],
+     "page": 10
+    },
+    {
+     "rect": [
+      218,
+      350,
+      349,
+      374
+     ],
+     "page": 23
+    },
+    {
+     "rect": [
+      29,
+      374,
+      348,
+      474
+     ],
+     "page": 11
+    },
+    {
+     "rect": [
+      348,
+      374,
+      349,
+      475
+     ],
+     "page": 11
+    },
+    {
+     "rect": [
+      28,
+      474,
+      29,
+      575
+     ],
+     "page": 11
+    },
+    {
+     "rect": [
+      29,
+      474,
+      348,
+      574
+     ],
+     "page": 11
+    },
+    {
+     "rect": [
+      29,
+      574,
+      349,
+      632
+     ],
+     "page": 11
+    },
+    {
+     "rect": [
+      167,
+      637,
+      218,
+      674
+     ],
+     "page": 7
+    },
+    {
+     "rect": [
+      42,
+      642,
+      78,
+      671
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      94,
+      642,
+      135,
+      671
+     ],
+     "page": 24
+    },
+    {
+     "rect": [
+      239,
+      642,
+      280,
+      671
+     ],
+     "page": 9
+    },
+    {
+     "rect": [
+      296,
+      642,
+      330,
+      668
+     ],
+     "page": 22
+    }
+   ],
+   "title": "22. User Profile Favorites",
+   "height": 795
+  },
+  {
+   "image": "23. User Profile Blogs.png",
+   "image2x": "23. User Profile Blogs@2x.png",
+   "width": 375,
+   "links": [
+    {
+     "rect": [
+      28,
+      350,
+      29,
+      375
+     ],
+     "page": 22
+    },
+    {
+     "rect": [
+      195,
+      685,
+      342,
+      700
+     ],
+     "page": 25
+    },
+    {
+     "rect": [
+      342,
+      685,
+      344,
+      741
+     ],
+     "page": 25
+    },
+    {
+     "rect": [
+      35,
+      685,
+      182,
+      702
+     ],
+     "page": 25
+    },
+    {
+     "rect": [
+      182,
+      685,
+      184,
+      743
+     ],
+     "page": 25
+    },
+    {
+     "rect": [
+      167,
+      637,
+      218,
+      674
+     ],
+     "page": 7
+    },
+    {
+     "rect": [
+      42,
+      642,
+      78,
+      671
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      94,
+      642,
+      135,
+      671
+     ],
+     "page": 24
+    },
+    {
+     "rect": [
+      239,
+      642,
+      280,
+      671
+     ],
+     "page": 9
+    },
+    {
+     "rect": [
+      296,
+      642,
+      330,
+      668
+     ],
+     "page": 22
+    }
+   ],
+   "title": "23. User Profile Blogs",
+   "height": 795
+  },
+  {
+   "image": "24. Home Blog.png",
+   "image2x": "24. Home Blog@2x.png",
+   "width": 375,
+   "links": [
+    {
+     "rect": [
+      167,
+      637,
+      218,
+      675
+     ],
+     "page": 7
+    },
+    {
+     "rect": [
+      43,
+      643,
+      78,
+      672
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      94,
+      643,
+      134,
+      672
+     ],
+     "page": 24
+    },
+    {
+     "rect": [
+      239,
+      643,
+      279,
+      672
+     ],
+     "page": 9
+    },
+    {
+     "rect": [
+      295,
+      643,
+      329,
+      670
+     ],
+     "page": 22
+    }
+   ],
+   "title": "24. Home Blog",
+   "height": 795
+  },
+  {
+   "image": "25. Blog Detail.png",
+   "image2x": "25. Blog Detail@2x.png",
+   "width": 375,
+   "links": [
+    {
+     "rect": [
+      167,
+      645,
+      218,
+      682
+     ],
+     "page": 7
+    },
+    {
+     "rect": [
+      43,
+      651,
+      79,
+      680
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      95,
+      651,
+      136,
+      680
+     ],
+     "page": 24
+    },
+    {
+     "rect": [
+      239,
+      651,
+      280,
+      680
+     ],
+     "page": 9
+    },
+    {
+     "rect": [
+      296,
+      651,
+      330,
+      678
+     ],
+     "page": 22
+    },
+    {
+     "rect": [
+      29,
+      144,
+      61,
+      176
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      175,
+      139,
+      215,
+      179
+     ],
+     "page": 15
+    }
+   ],
+   "title": "25. Blog Detail",
+   "height": 795
+  }
+ ],
+ "resolutions": [2],
+ "title": "Storyboard",
+ "highlightLinks": false
+}
